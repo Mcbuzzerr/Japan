417884d53b0d931e0bc33c3db10cc32ee638b830
Alex <mcbuzzer@gmail.com>
2018-04-15 17:23:42 -0600

    Did a thing

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,16 @@ class PagesController < ApplicationController
   def home
  	
   end
+  def geo
+  	
+  end
+  def city
+  	
+  end
+  def history
+  	
+  end
+  def culture
+  	
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "pages#home"
+
+  get '/geography', to: 'pages#geo'
+  get '/city', to: 'pages#city'
+  get '/history', to: 'pages#history'
+  get '/culture', to: 'pages#culture'
 end
